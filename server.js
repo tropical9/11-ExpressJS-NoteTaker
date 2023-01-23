da953e79b1157bf73336826594e2d51aa9756235
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-01-22 22:26:49 -0800

    adding get

--- server.js
+++ server.js
@@ -1,41 +1,48 @@
-const express = require ('express');
-const path = require ('path');
-const fs = require ('fs');
+const express = require("express");
+const path = require("path");
+const fs = require("fs");
+const notes = require("./db/db.json");
 
-const notes = require ('./db/db.json');
-const uuid = require ('uuid');
-const app = express ();
+const uuid = require('uuid');
 
-const PORT = process.env.PORT || 3000;
+
+const app = express();
+
+var PORT = process.env.PORT || 3000;
+
 
 app.use(express.json());
-app.use(express.urlencoded({extended: true}));
-app.use(express.static('public'));
+app.use(express.urlencoded({ extended: true }));
 
 
-app.get("api/notes", (req, res)=>
+app.use(express.static("public"));
+
+//should read the db.json file and return all saved notes as JSON
+app.get("/api/notes", (req, res) => 
     res.sendFile(path.join(__dirname, "/db/db.json"))
-    );
+);
 
-
-
-app.post ("/api/notes", (req, res)=> {
-    const notes = JSON.parse(fs.readFyleSync("./db/db.json"));
+//should receive a new note to save on the request body, add it to the db.json file, and then return the new note to the client. 
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
     const newNotes = req.body;
     newNotes.id = uuid.v4();
     notes.push(newNotes);
     fs.writeFileSync("./db/db.json", JSON.stringify(notes))
     res.json(notes);
 });
-    
+
+// should receive a query parameter containing the id of a note to delete, read all notes from the db.json file, remove the note with the given id property, and then rewrite the db.json.
 app.delete("/api/notes/:id", (req, res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const dltNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id); // deletes a specific note
+    const dltNote = notes.filter((rmvNote) => rmvNote.id !== req.params.id);
     fs.writeFileSync("./db/db.json", JSON.stringify(dltNote));
     res.json(dltNote);
 })
 
-app.get("/", function (req, res) {
+
+
+app.get("*", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
@@ -48,4 +55,3 @@ app.get("/notes", function (req, res) {
 app.listen(PORT, function () {
     console.log(`Example app listening at http://localhost:${PORT} 🚀`)
 });
-    
