7efa61315be620803365dadcaa7c730d6c1e8b83
tropical9 <113566952+tropical9@users.noreply.github.com>
2023-01-22 23:50:46 -0800

    server

--- server.js
+++ server.js
@@ -7,26 +7,9 @@ const app = express();
 
 var PORT = process.env.PORT || 3000;
 
-
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
 app.use(express.static("public"));
-
-app.get("/api/notes", (req, res) => 
-    res.sendFile(path.join(__dirname, "/db/db.json"))
-);
-
-
-app.post("/api/notes", (req, res) => {
-    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
-    const newNotes = req.body;
-    newNotes.id = uuid.v4(); 
-    notes.push(newNotes);
-    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
-    res.json(notes);
-});
-
 
 app.delete("/api/notes/:id", (req, res) => {
     const notes = JSON.parse(fs.readFileSync("./db/db.json"));
@@ -34,16 +17,27 @@ app.delete("/api/notes/:id", (req, res) => {
     fs.writeFileSync("./db/db.json", JSON.stringify(dltNote));
     res.json(dltNote);
 })
-
-
-
+//should return index html
 app.get("/", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/index.html"));
 });
 
-
+//should return notes html
 app.get("/notes", function (req, res) {
     res.sendFile(path.join(__dirname, "/public/notes.html"));
+});
+//should return db 
+app.get("/api/notes", (req, res) => 
+    res.sendFile(path.join(__dirname, "/db/db.json"))
+);
+//should return new note
+app.post("/api/notes", (req, res) => {
+    const notes = JSON.parse(fs.readFileSync("./db/db.json"));
+    const newNotes = req.body;
+    newNotes.id = uuid.v4(); 
+    notes.push(newNotes);
+    fs.writeFileSync("./db/db.json", JSON.stringify(notes))
+    res.json(notes);
 });
 
 app.listen(PORT, function () {
